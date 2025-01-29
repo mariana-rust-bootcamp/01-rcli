57f07289dfa68a88e8b8782ee461b9fe1ab79b55
Mariana <HealMSlin@gmail.com>
2025-01-29 22:40:20 +0800

    feat: add csv cli support

---
+++ src/opts.rs
@@ -0,0 +1,39 @@
+use std::path;
+
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+#[command(name = "rcli", version, author, about, long_about = None)]
+pub struct Opts {
+    #[command(subcommand)]
+    pub cmd: SubCommand,
+}
+
+#[derive(Debug, Parser)]
+pub enum SubCommand {
+    #[command(name = "csv", about = "Show CSV, or convert CSV to other formats.")]
+    Csv(CsvOpts),
+}
+
+#[derive(Debug, Parser)]
+pub struct CsvOpts {
+    #[arg(short, long, value_parser = verify_input_file)]
+    pub input: String,
+
+    #[arg(short, long, default_value = "output.json")]
+    pub output: String,
+
+    #[arg(short, long, default_value_t = ',')]
+    pub delimiter: char,
+
+    #[arg(long, default_value_t = true)]
+    pub header: bool,
+}
+
+fn verify_input_file(file: &str) -> Result<String, &'static str> {
+    if path::Path::new(file).exists() {
+        Ok(file.into()) // 切片 -> 含有所有权的String
+    } else {
+        Err("File does not exist")
+    }
+}
