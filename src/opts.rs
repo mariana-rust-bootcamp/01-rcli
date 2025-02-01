ce4664fbf047c0fc23d0b34f0ef7523e29d8c7e9
Mariana <HealMSlin@gmail.com>
2025-02-02 00:20:01 +0800

    feat: support generic csv to json and yaml output and add password generator

--- src/opts.rs
+++ src/opts.rs
@@ -1,4 +1,4 @@
-use std::path;
+use std::{fmt, path, str::FromStr};
 
 use clap::Parser;
 
@@ -9,19 +9,31 @@ pub struct Opts {
     pub cmd: SubCommand,
 }
 
+#[derive(Debug, Clone, Copy)]
+pub enum OutputFormat {
+    Json,
+    Yaml,
+}
+
 #[derive(Debug, Parser)]
 pub enum SubCommand {
     #[command(name = "csv", about = "Show CSV, or convert CSV to other formats.")]
     Csv(CsvOpts),
+    #[command(name = "genpass", about = "Generate a random password")]
+    GenPass(GenPassOpts),
 }
 
+/// 文本格式转换
 #[derive(Debug, Parser)]
 pub struct CsvOpts {
     #[arg(short, long, value_parser = verify_input_file)]
     pub input: String,
 
-    #[arg(short, long, default_value = "output.json")]
-    pub output: String,
+    #[arg(short, long)] // "output.json".into()
+    pub output: Option<String>,
+
+    #[arg(long, value_parser = parse_format, default_value = "json")]
+    pub format: OutputFormat,
 
     #[arg(short, long, default_value_t = ',')]
     pub delimiter: char,
@@ -30,10 +42,56 @@ pub struct CsvOpts {
     pub header: bool,
 }
 
+/// 生成密码
+#[derive(Debug, Parser)]
+pub struct GenPassOpts {
+    #[arg(short, long, default_value_t = 16)]
+    pub length: u8,
+    #[arg(long, default_value_t = true)]
+    pub uppercase: bool,
+    #[arg(long, default_value_t = true)]
+    pub lowercase: bool,
+    #[arg(long, default_value_t = true)]
+    pub number: bool,
+    #[arg(long, default_value_t = true)]
+    pub symbols: bool,
+}
+
 fn verify_input_file(file: &str) -> Result<String, &'static str> {
     if path::Path::new(file).exists() {
         Ok(file.into()) // 切片 -> 含有所有权的String
     } else {
         Err("File does not exist")
+    }
+}
+
+fn parse_format(format: &str) -> Result<OutputFormat, anyhow::Error> {
+    format.parse()
+}
+
+impl FromStr for OutputFormat {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "json" => Ok(OutputFormat::Json),
+            "yaml" => Ok(OutputFormat::Yaml),
+            _ => Err(anyhow::anyhow!("Invalid format")),
+        }
+    }
+}
+/// format!需要参数类型实现fmt::Display trait
+impl fmt::Display for OutputFormat {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", Into::<&str>::into(*self)) // Into::into() 用于将类型转换成另一个类型, 需要将参数实现From方法
+    }
+}
+/// Into::into需要参数实现From trait
+impl From<OutputFormat> for &'static str {
+    fn from(value: OutputFormat) -> Self {
+        match value {
+            OutputFormat::Json => "json",
+            OutputFormat::Yaml => "yaml",
+        }
     }
 }
---
+++ src/process/gen_pass.rs
@@ -0,0 +1,49 @@
+use anyhow::Result;
+use rand::seq::{IndexedRandom, SliceRandom};
+
+const UPPER: &[u8] = b"ABCDEFGHJKLMNPQRSTUVWXYZ";
+const LOWER: &[u8] = b"abcdefghjklmnpqrstuvwxyz";
+const NUMBER: &[u8] = b"23456789";
+const SYMBOL: &[u8] = b"!@#$%^&*_";
+
+pub fn process_genpass(
+    length: u8,
+    upper: bool,
+    lower: bool,
+    number: bool,
+    special: bool,
+) -> Result<()> {
+    let mut rng = rand::rng();
+    let mut password = Vec::new();
+    let mut chars = Vec::new();
+    // 这里用UPPER.choose获得的是&u8, 需要*UPPER才能获得u8
+    if upper {
+        chars.extend_from_slice(UPPER);
+        password.push(*UPPER.choose(&mut rng).expect("UPPER won't be empty"));
+    }
+    if lower {
+        chars.extend_from_slice(LOWER);
+        password.push(*LOWER.choose(&mut rng).expect("LOWER won't be empty"));
+    }
+    if number {
+        chars.extend_from_slice(NUMBER);
+        password.push(*NUMBER.choose(&mut rng).expect("NUMBER won't be empty"));
+    }
+    if special {
+        chars.extend_from_slice(SYMBOL);
+        password.push(*SYMBOL.choose(&mut rng).expect("SYMBOL won't be empty"));
+    }
+    for _ in 0..(length - password.len() as u8) {
+        let c = chars
+            .choose(&mut rng)
+            .expect("chars won't be empty in this context");
+        password.push(*c);
+    }
+
+    password.shuffle(&mut rng);
+
+    // String原生支持{} 不需要{:?}
+    println!("{}", String::from_utf8(password)?);
+
+    Ok(())
+}
